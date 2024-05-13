0d4b1f28f79761921aa9b6ab0077a1b415dc58c7
tareqhasan382 <tareqhasan382@gmail.com>
2024-05-13 21:39:07 +0600

    added Backend URL

--- src/Redux/api/baseApi.ts
+++ src/Redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8000/api/v1",
+    baseUrl: "https://hotel-booking-backent.vercel.app/api/v1",
     prepareHeaders: (headers) => {
       const authString = localStorage.getItem("auth");
       const auth = authString ? JSON.parse(authString) : null;
